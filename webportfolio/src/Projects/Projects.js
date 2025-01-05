9e74db8bcdf4daac330652ac84544ff3141bbcbf
Terry Lu <t.lu@ufl.edu>
2025-01-05 00:31:28 -0500

    Projects Section

--- webportfolio/src/Projects/Projects.js
+++ webportfolio/src/Projects/Projects.js
@@ -3,8 +3,48 @@ import "./Projects.css"
 const Projects = () => {
   return (
     <section id ="projects">
-        <h2>Projects</h2>
-        <p>FreshWatch</p>
+        <h2>Recent Projects</h2>
+        <div className="project-box">
+          <div>
+            <h3>
+              FreshWatch
+            </h3>
+            <p>
+              Put some text here
+            </p>
+          </div>
+          <div>
+            Put an Image Here
+          </div>
+        </div>
+
+        <div className="project-box">
+          <div>
+            <h3>
+              FreshWatch
+            </h3>
+            <p>
+              Put some text here
+            </p>
+          </div>
+          <div>
+            Put an Image Here
+          </div>
+        </div>
+
+        <div className="project-box">
+          <div>
+            <h3>
+              FreshWatch
+            </h3>
+            <p>
+              Put some text here
+            </p>
+          </div>
+          <div>
+            Put an Image Here
+          </div>
+        </div>
       </section>
   )
 }
