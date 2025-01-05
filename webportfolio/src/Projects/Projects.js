1ca9561ef2d7534180e8c4a20751d373b09bac90
Terry Lu <t.lu@ufl.edu>
2025-01-05 01:35:06 -0500

    More work on Projects Section

--- webportfolio/src/Projects/Projects.js
+++ webportfolio/src/Projects/Projects.js
@@ -1,4 +1,7 @@
 import "./Projects.css"
+import FreshWatch from "../Images/FreshWatch.png"
+import Shiftflow from "../Images/Timer.jpg"
+import Crimes from "../Images/Crimes.png"
 
 const Projects = () => {
   return (
@@ -10,39 +13,54 @@ const Projects = () => {
               FreshWatch
             </h3>
             <p>
-              Put some text here
+              Built a full-stack grocery tracking application to provide an efficient way for users to track their groceries and expiration dates. Used MongoDB, ExpressJS, ReactJS, and NodeJS.
             </p>
+            <a href="https://github.com/terrylu12212/FreshWatch-Terry-" target="_blank"  rel="noreferrer">
+            <button>
+              View Github
+            </button>
+            </a>
           </div>
-          <div>
-            Put an Image Here
+          <div className="image-container">
+            <img src= {FreshWatch} alt="FreshWatch" className="image"/>
           </div>
         </div>
 
         <div className="project-box">
           <div>
             <h3>
-              FreshWatch
+              ShiftFlow Master
             </h3>
             <p>
-              Put some text here
+              Collaborated with a team to create a full-stack time-tracking application for employees and managers to log time and projects. Created using MongoDB, ExpressJS, ReactJS, and NodeJS.
             </p>
+            <a href="https://github.com/terrylu12212/ShiftFlow-Master" target="_blank"  rel="noreferrer">
+            <button>
+              View Github
+            </button>
+            </a>
           </div>
-          <div>
-            Put an Image Here
+          <div className="image-container">
+            <img src= {Shiftflow} alt="ShiftFLow Master" className="image"/>
           </div>
         </div>
 
         <div className="project-box">
           <div>
             <h3>
-              FreshWatch
+              Chicago Crimes 
             </h3>
             <p>
-              Put some text here
+              Project developed in C++ to display districts in Chicago that had the highest crime rates, based on specific user input. Implemented sorting algorithms such as TimSort.
             </p>
+            <a href="https://github.com/terrylu12212/Chicago-Crime-Project" target="_blank"  rel="noreferrer">
+            <button>
+              View Github
+            </button>
+            </a>
           </div>
-          <div>
-            Put an Image Here
+          <div className="image-container">
+            <img src= {Crimes} alt="Chicago Crimes Project" className="image"/>
           </div>
         </div>
       </section>
