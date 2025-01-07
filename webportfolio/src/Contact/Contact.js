848baa1b841fb2afc3158bf32200f876d86e2771
Terry Lu <t.lu@ufl.edu>
2025-01-06 20:29:30 -0500

    Worked on Contact Form and Finished Sending Email functionality via EmailJS

--- webportfolio/src/Contact/Contact.js
+++ webportfolio/src/Contact/Contact.js
@@ -1,9 +1,96 @@
 import "./Contact.css"
+import {useState} from 'react';
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+  const [name, setName] = useState('')
+  const[email, setEmail] = useState('')
+  const[message, setMessage] = useState('')
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+    console.log(name)
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+    console.log(email)
+  }
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value)
+    console.log(message)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name)
+    console.log(email)
+    console.log(message)
+    const serviceId = "service_g6cns1m"
+    const templateId = "template_5c4iejv"
+    const publicKey = "9KU5jdLthGiLChAHF"
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "Terry",
+      message: message,
+    }
+
+    emailjs.send(serviceId, templateId, templateParams, publicKey).then((response) =>{
+      console.log("Email sent successfully", response)
+      setName('')
+      setEmail('')
+      setMessage('')
+    }).catch((error) => {
+      console.error('Error sending email:', error)
+    })
+
+  }
+
   return (
     <section id="contact-me">
-      This is the Contact Section
+      <h2>Contact Me</h2>
+      <h3>Contact me directly at terrylu794@gmail.com</h3>
+      <div className="form-container">
+      <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          id="name"
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+          onChange={handleNameChange}
+          required
+        />
+        </div>
+
+        <div>
+        <input
+          id="email"
+          type="text"
+          placeholder="What's your email?"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+        </div>
+
+        <div>
+        <input
+          id="message"
+          type="textarea"
+          placeholder="What's your message?"
+          value={message}
+          onChange={handleMessageChange}
+          required
+        />
+        </div>
+        <button type = "Submit">Send</button>
+      </form>
+      </div>
+
     </section>
   )
 }
